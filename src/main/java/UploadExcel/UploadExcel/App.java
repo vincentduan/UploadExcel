72525a1d9c2cc68343be0af3bf20e3e2a105290c
vincent_duan <1621424380@qq.com>
2016-03-27 03:30:09 +0800

    init project

---
+++ src/main/java/UploadExcel/UploadExcel/App.java
@@ -0,0 +1,13 @@
+package UploadExcel.UploadExcel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
